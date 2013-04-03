189929f89ca2e927a65d8ee6523bf082edcfe6e8
Taylor Solomon <noliteloqui@gmail.com>
2013-04-03 16:35:46 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Prayerstack::Application.config.secret_token = 'a1b4cd5c0157a4fc4012f1846635e90dc9f874bd08bb458eb34b019a361885d59a034bf8d49a2465de6416f4654f0f252c4460e10a99230474915ae8e2c545e8'
